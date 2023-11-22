371553e197201ea5ed0d2dde24435927b2b3f764
erikayeah <erikafogar.j@gmail.com>
2023-11-22 14:26:35 +0100

    favorites

--- Front/src/components/card/Card.jsx
+++ Front/src/components/card/Card.jsx
@@ -1,19 +1,63 @@
 import style from '../card/Card.module.css';
 import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {removeFav, addFav} from '../../redux/action'
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 
-const Card = ({name, id, status, species, gender, origin, image, onClose}) => { //destructuring de las pripierdades del objeto props. si no seria props sin {}, y abajo props.name y asi.
+const Card = ({name, id, image, onClose}) => { //destructuring de las pripierdades del objeto props. si no seria props sin {}, y abajo props.name y asi.
+   
+   const dispatch = useDispatch();
+   const myFavorites = useSelector(state => state.myFavorites); //Tmb {myFavorites = useSelector (state => state)}, con destructuring.
+   const {pathname} = useLocation();
+   const [isFav, setIsFav] = useState(false);
+
+   useEffect(() => {
+      myFavorites.forEach((fav) => {
+        if (fav.id === id) {
+          setIsFav(true);
+        }
+      });
+    }, [myFavorites]);
+
+/* Version bucle for
+useEffect(() => {
+for ( let i = 0; i < myFavorites.length; i ++) {
+if (myFavorites[i].id === props.id) {
+   setIsFav(true)
+}}
+}, [myFavorites, props.id]
+*/
+
+   const handleFavorite = () => {
+      if (isFav) {
+         setIsFav(false);
+         dispatch(removeFav(id)); //Si yo no hubiera hecho destructuring ariba, esto seria props.id
+      } else {
+         setIsFav(true);
+         dispatch(addFav({name, id, image, onClose})); //Mismo caso si no hubiera hecho destructuring, seria props solo.
+      }
+   };
+/*Version en ternario
+   const handleFavorite = () => {
+   isFav ? (setIsFav(false), dispatch(removeFav(id))) : (setIsFav(true), dispatch(addFav(props)));
+}; */
+
+   
    return (
       <div className = {style.card}>
-         <button onClick={() => onClose(id) }> X </button>
+
+         {isFav ? (<button onClick={handleFavorite}>❤️</button>) 
+         : (<button onClick={handleFavorite}>🤍</button>)}
+
+         {/* {pathname !== '/favorites' ? <button onClick={() => onClose(id) }> X </button> : ''} */}
+         < button onClick={() => onClose(id) }> X </button>
+
           <h2> {name}</h2>
          <Link to={`/detail/${id}`} >
          <img src={image} alt={name} />
          </Link>
-         {/* <h3> Status: {status}</h3>
-         <h3> Species: {species}</h3>
-         <h3> Gender: {gender}</h3>
-         <h3> Origin: {origin.name}</h3> */}
       </div>
    );
 }
