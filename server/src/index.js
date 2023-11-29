8ba9b381fe794b5cc6b893688ad107fa171efdb3
erikayeah <erikafogar.j@gmail.com>
2023-11-29 14:35:30 +0100

    server

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,36 @@
 const http = require("http");
-const data = require('./utils/data')
+const PORT = 3001;
+const characters = require('./utils/data')
 
 http.createServer((req, res) => {
-   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Origin', '*'); //Le da acceso al front. Con * digo q cualquiera puede acceder.
 
    if (req.url.includes('/rickandmorty/character')){
 
-      const id = req.url.split('/').pop(); // o metodo pop.
-      const characterFound = data.find((character) => character.id === Number(id));
+      const id = req.url.split('/').pop(); //.at(-1) tambien.
+      const character = characters.find( //Con filter no nada, dsp veamos porque
+         char => char.id === Number(id)
+         );
 
-      if (characterFound) {
-         return res 
-            .writeHead(200, {'contenty-type': 'application/json'})
-            .end(JSON.stringify(characterFound));
-      }
+         if (character) {
+            return res
+               .writeHead(200, {'Content-Type': 'application/json'})
+               .end(JSON.stringify(character));
+         } else {
 
-      return res
-         .writeHead(404, {'content-type' : 'application/json'})
-         .end(JSON.stringify({error: 'No existen personajes con ese ID'}));
-      }
-}).listen(3001, '127.0.0.1');
+            return res
+               .writeHead(404, {'Content-Type': 'application/json'})
+               .end(JSON.stringify({message: 'No existen personajes con ese ID'}));
+         }
+    }
+
+    return res
+         .writeHead(404, {'Content-Type': 'application/json'})
+         .end(JSON.stringify({message: 'Esa URL no existe'}));
+
+})
+   .listen(PORT, '127.0.0.1',
+   () => (console.log(`Server listening on port ${PORT}`)) //Este es un cb extra que nos dio el profe. un mensajito extra.
+); //Siempre cierro con .listen y debo pasarle el port, y la mascara de subred. el numero, o localhost.
+
+
