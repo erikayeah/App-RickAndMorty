76c92b0dcea89bce7f60793de02fa9da9857b90a
erikayeah <erikafogar.j@gmail.com>
2023-12-05 14:31:10 +0100

    Async Await

--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -3,26 +3,60 @@ const axios = require("axios");
 const URL = "https://rickandmortyapi.com/api/character";
 // const API_KEY = "henrystaff";
 
-const getCharById = (req, res) => {
-  const { id } = req.params;
-  axios
-    .get(`${URL}/${id}`) //* promise pending
-    .then(({ data }) => {
-      //* data = { id:1, name:"Rick", ... }
-      const {
-        id, status, name, species, origin, image, gender, location
-      } = data;
-      const character = {
-        id, status, name, species, origin, image, gender, location
-      };
-      //* character = { id, name, ... }
-      return character.name
+const getCharById = async (req, res) => {
+  try {
+    const characterId = req.params.id;
+    const { data } = await axios.get(`${URL}/${characterId}`);
+    const {
+      id, status, name, species, origin, image, gender, location
+    } = data;
+    const character = {
+      id, status, name, species, origin, image, gender, location
+    };
+    return character.name
         ? res.json(character)
-        : res.status(404).send("Not fount")
-    })
-    .catch(error => {
-      return res.status(500).send(error.message);
-    });
+        : res.status(404).send("Not found")
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 };
 
 module.exports = getCharById;
+
+
+
+
+
+
+
+
+
+// const getCharById =  async (req, res) => {
+  
+//   try { 
+//     const { id } = req.params;
+//     const {data } = await axios.get(`${URL}/${id}`); //* promise pending
+//       //* data = { id:1, name:"Rick", ... }
+   
+//       const character = {
+//         id: data.id, 
+//         status: data.status, 
+//         name: data.name, 
+//         species: data.species, 
+//         origin: data.origin, 
+//         image: data.image , 
+//         gender: data.gender, 
+//         location: data.location
+//       };
+
+//       if (character.name) {
+//         return res.json(character);
+//       } else {
+//         return res.status(404).send("Not found");
+//       }
+//     } catch (error) {
+//         return res.status(500).send(error.message);
+//       }
+//     }
+
+
