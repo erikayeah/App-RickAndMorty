dfc6c2d388bb88116fa9052f75dfebba3170e489
ErikaFogar <erikafogar.j@gmail.com>
2024-02-02 11:19:26 +0100

    detail

--- client/src/components/detail/Detail.jsx
+++ client/src/components/detail/Detail.jsx
@@ -8,7 +8,7 @@ const Detail = () => {
   const [character, setCharacter] = useState({});
 
   useEffect(() => {
-    axios(`http://localhost:3001/rickandmorty/character/${id}`).then(
+    axios(`http://localhost:1000/rickandmorty/character/${id}`).then(
       ({ data }) => {
         if (data.name) {
           setCharacter(data);
